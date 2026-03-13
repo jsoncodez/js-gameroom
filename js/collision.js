b725f1fab77d0a22042e6d2634dd3185c0051462
jscodez <jason20@umbc.edu>
2026-03-13 07:02:29 -0400

    Implement collision detection function

---
+++ js/collision.js
@@ -0,0 +1,10 @@
+function isColliding(a, b) {
+
+    return (
+        a.x < b.x + b.width &&
+        a.x + a.width > b.x &&
+        a.y < b.y + b.height &&
+        a.y + a.height > b.y
+    );
+
+}
